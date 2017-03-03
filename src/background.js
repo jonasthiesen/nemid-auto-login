4366c3c5a759cc5919aa8c8d01b89fc0fe6e0977
Jonas Thiesen <jonas.k.thiesen@gmail.com>
2017-03-03 16:17:14 +0100

    updates to files and add popup.html and popup.html

--- src/background.js
+++ src/background.js
@@ -1,7 +1,3 @@
 const Tesseract = require('tesseract.js');
 
-Tesseract.recognize("icon.png")
-		 .progress(function (p) { console.log('progress', p) })
-		 .then( function (result) { console.log('result', result) })
-
 chrome.browserAction.onClicked.addListener(function(tab) { alert("test")});
